19dbe4dc1376011177476d0ddeb1ee3ae39c57ae
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-04-11 18:16:08 +0200

    aggiunta stampa snack 4 (può guidare verde,non può guidare rosso)

--- Snack-4/main.js
+++ Snack-4/main.js
@@ -39,23 +39,27 @@ const persone = [{
 
 const persone_2 = persone.map(persona => {
     if (persona.eta > 17) {
-        return `${persona.cognome} ${persona.nome} ha ${persona.eta}, quindi può guidare`
+        return `
+        <div class="card" style="width: calc(100% - 2rem); background-color: green">
+        <h2>${persona.cognome} ${persona.nome} ha ${persona.eta}, quindi può guidare</h2>
+        </div>
+        `
     } else {
-        return `${persona.cognome} ${persona.nome} ha ${persona.eta}, quindi non può guidare`
+        return `
+        <div class="card" style="width: calc(100% - 2rem); background-color: red">
+        <h2>${persona.cognome} ${persona.nome} ha ${persona.eta}, quindi può guidare</h2>
+        </div>
+        `
     }
 })
 console.log(persone_2);
 
 stamp_list('.cards', persone_2)
 
+
 function stamp_list(dom_element, array) {
     const element_cards = document.querySelector(dom_element)
     array.forEach(element => {
-        const markup = `
-      <div class="card" style="width: calc(100% - 2rem)">
-          <h2>${element}</h2>
-      </div>
-      `
-        element_cards.insertAdjacentHTML('beforeend', markup)
+        element_cards.insertAdjacentHTML('beforeend', element)
     });
 }
